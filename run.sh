5eed81e39e771914547cbb835fe6c30784330324
helloyongyang <yongyang1030@163.com>
2024-05-14 15:18:50 +0800

    update

--- run.sh
+++ run.sh
@@ -20,8 +20,7 @@ main.py \
 --tag $save_name \
 --output ./save \
 --sub_data_size 10240 \
---use_intel_layer_loss \
---KD_loss MSE \
+--KD_loss KL \
 --opts TRAIN.EPOCHS 100 TRAIN.BASE_LR 0.04 TRAIN.WEIGHT_DECAY 3e-4 TRAIN.WARMUP_EPOCHS 1 MODEL.LABEL_SMOOTHING 0.1 AUG.PRESET weak AUG.MIXUP 0.0 DATA.DATASET imagenet DATA.IMG_SIZE 224 SAVE_FREQ 1 \
 EXTRA.sInit_value -8 EXTRA.target_sparsity 0.70 EXTRA.sparsity_loss_type abs EXTRA.sparsity_lambda 1.0 \
 EXTRA.st_decay 0.0 \
